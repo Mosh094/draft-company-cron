2b85e3eeeabdbee946a620f9105ad64bb2357b95
cmdevops-at-196838470078 <mash4real@yahoo.com>
2023-05-11 22:54:03 +0100

    workign script

--- company-cron.py
+++ company-cron.py
@@ -1,64 +1,61 @@
-import psycopg2
-import openai
 import os
-import re
-from urllib.parse import urlparse
+import openai
+import psycopg2
+import psycopg2.extras
 import json
+import re
 from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()
 
+database_url = os.getenv("DATABASE_URL")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-def load_gics_data(json_file):
-    with open(json_file, "r") as f:
-        gics_data = json.load(f)
+# Connect to your postgres DB
+conn = psycopg2.connect(database_url, sslmode='require')
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    return gics_data
+# Set OpenAI API key
+openai.api_key = openai_api_key
 
-
-gics_data = load_gics_data("gics_data.json")
-
-
-def find_and_validate_gics_value(gics_value, gics_data, gics_column):
-    for entry in gics_data:
-        if gics_value.lower() in entry[gics_column].lower():
-            return entry[gics_column]
-
-    return None
-
-
-def find_and_validate_gics_value(gics_response, gics_data, gics_column):
-    for entry in gics_data:
-        if entry[gics_column].lower() in gics_response.lower():
-            return entry[gics_column]
-
-    return None
-
-
-# Updating database credential
-DB_NAME = "df7n96sahm849q"
-DB_USER = "ejesvaacpduvgt"
-DB_PASSWORD = "797dbf4dcf91d5c399df0a719cf7d73c99f6fb3bc09d7fc384627833e89ca9f9"
-DB_HOST = "ec2-34-197-84-74.compute-1.amazonaws.com"
-DB_PORT = "5432"
-
-# Setting OpenAI API-key
-openai.api_key = "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"
+# Load GICS data
+with open('gics_data.json', 'r') as f:
+    gics_data = json.load(f)
 
 
 def get_missing_data():
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
-                            password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
-    cursor = conn.cursor()
+    cur.execute(
+        "SELECT * FROM companies WHERE url IS NULL OR linkedin_url IS NULL OR gics_sub_industries IS NULL;")
+    return cur.fetchall()
 
-    query = """
-        SELECT id, name, url, linkedin_url, gics_sectors, gics_industry_groups, gics_industries, gics_sub_industries
-        FROM company
-        WHERE url IS NULL OR linkedin_url IS NULL OR gics_sectors IS NULL OR gics_industry_groups IS NULL
-            OR gics_industries IS NULL OR gics_sub_industries IS NULL;
-    """
-    cursor.execute(query)
-    return cursor.fetchall()
+
+def update_data(id, column, value):
+    try:
+        cur.execute(
+            f"UPDATE companies SET {column} = %s WHERE id = %s;", (value, id))
+        conn.commit()
+    except psycopg2.Error as e:
+        print(f"An error occurred while updating the database: {e}")
+
+
+def get_chatgpt_response(prompt, response_type):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=50,
+    )
+
+    message = response.choices[0].text.strip()
+
+    if message.lower() in ["i don't know", "i am not sure", "i cannot answer that", "i cannot answer that", "is not known", "is not found", "is not available", "I do not know"]:
+        return None
+    else:
+        if response_type == "url" or response_type == "linkedin_url":
+            return extract_url(message)
+        elif response_type in ["gics_sectors", "gics_industry_groups", "gics_industries", "gics_sub_industries"]:
+            return extract_gics(message)
+        else:
+            return message
 
 
 def extract_url(response_text):
@@ -78,69 +75,60 @@ def extract_gics(response_text):
     return ' '.join(cleaned_words)
 
 
-def get_chatgpt_response(prompt, response_type):
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+def find_and_validate_gics_value(gics_value, gics_data, gics_column):
+    for entry in gics_data:
+        if gics_value.lower() in entry[gics_column].lower():
+            return entry[gics_column]
 
-    message = response.choices[0].text.strip()
-
-    if message.lower() in ["i don't know", "i am not sure", "i cannot answer that", "i cannot answer that", "is not known", "is not found", "is not available", "I do not know"]:
-        return None
-    else:
-        if response_type == "url" or response_type == "linkedin_url":
-            return extract_url(message)
-        elif response_type in ["gics_sectors", "gics_industry_groups", "gics_industries", "gics_sub_industries"]:
-            return extract_gics(message)
-        else:
-            return message
+    return None
 
 
-def update_data(id, column_name, value):
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
-                            password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
-    cursor = conn.cursor()
+def find_and_validate_gics_value(gics_response, gics_data, gics_column):
+    for entry in gics_data:
+        if entry[gics_column].lower() in gics_response.lower():
+            return entry[gics_column]
 
-    query = f"UPDATE company SET {column_name} = %s WHERE id = %s;"
-    cursor.execute(query, (value, id))
-    conn.commit()
+    return None
 
 
 def main():
-    missing_data = get_missing_data()
+    try:
+        missing_data = get_missing_data()
 
-    for data in missing_data:
-        id = data[0]
-        name = data[1]
+        for data in missing_data:
+            id = data['id']
+            name = data['name']
 
-        url = data[2]
-        linkedin_url = data[3]
-        gics_sectors = data[4]
-        gics_industry_groups = data[5]
-        gics_industries = data[6]
-        gics_sub_industries = data[7]
+            url = data['url']
+            if url is None:
+                print(f"Updating URL for company {name}")
+                prompt = f"What is the website URL of the company '{name}'?"
+                url_response = get_chatgpt_response(prompt, 'url')
+                if url_response:
+                    print(f"URL response for company {name}: {url_response}")
+                    cleaned_url = extract_url(url_response)
+                    if cleaned_url:
+                        update_data(id, "url", cleaned_url)
+                print(f"Finished updating URL for company {name}")
 
-        if url is None:
-            prompt = f"What is the website URL of the company '{name}'?"
-            url = get_chatgpt_response(prompt, "url")
-            update_data(id, "url", url)
+            linkedin_url = data['linkedin_url']
+            if linkedin_url is None:
+                print(f"Updating LinkedIn URL for company {name}")
+                prompt = f"What is the LinkedIn URL of the company '{name}'?"
+                linkedin_url = get_chatgpt_response(prompt, "linkedin_url")
+                update_data(id, "linkedin_url", linkedin_url)
 
-        if linkedin_url is None:
-            prompt = f"What is the LinkedIn URL of the company '{name}'?"
-            linkedin_url = get_chatgpt_response(prompt, "linkedin_url")
-            update_data(id, "linkedin_url", linkedin_url)
-
-        if gics_sub_industries is None:
-            prompt = f"Can you classify '{name}' based on the Global Industry Classification Standard (GICS) classification under gics_sub_industries?"
-            gics_sub_industries_response = get_chatgpt_response(
-                prompt, "gics_sub_industries")
-            gics_sub_industries = find_and_validate_gics_value(
-                gics_sub_industries_response, gics_data, "gics_sub_industries")
+            gics_sub_industries = data['gics_sub_industries']
+            if gics_sub_industries is None:
+                print(f"Updating GICS sub-industry for company {name}")
+                prompt = f"Can you classify '{name}' based on the Global Industry Classification Standard (GICS) classification under gics_sub_industries?"
+                gics_sub_industries_response = get_chatgpt_response(
+                    prompt, "gics_sub_industries")
+                gics_sub_industries = find_and_validate_gics_value(
+                    gics_sub_industries_response, gics_data, "gics_sub_industries")
+                if gics_sub_industries is not None:
+                    print(
+                        f"GICS sub-industry response for company {name}: {gics_sub_industries}")
 
             if gics_sub_industries is not None:
                 for entry in gics_data:
@@ -153,12 +141,17 @@ def main():
                         update_data(id, "gics_sub_industries",
                                     entry["gics_sub_industries"])
                         break
+                print(f"Finished updating GICS data for company {name}")
+
+        print("Data update complete.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        cur.close()
+        conn.close()
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(f"Error: {e}")
-        import traceback
-        print(traceback.format_exc())
+    main()
